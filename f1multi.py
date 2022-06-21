6c1d89a37b5ba912061f7850c650be44f1e20d98
rajuptvs <raju.ptvs@gmail.com>
2022-06-21 12:15:34 -0400

    racepace Viz bug fix

--- f1multi.py
+++ f1multi.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from pages import f1,telemetrycomp
+from pages import f1,telemetrycomp,racepace
 
 app = MultiApp()
 
@@ -8,6 +8,7 @@ app = MultiApp()
 # Add all your application here
 app.add_app("Pace Difference vs Leader", f1.app)
 app.add_app("Telemetry Comparision", telemetrycomp.app)
+app.add_app("Race Pace", racepace.app)
 
 
 # The main app
